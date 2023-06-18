7cec0f8725aa72812860232930121c73717f6855
Matheus Filipe <theuslipe26@gmail.com>
2023-06-18 13:52:12 -0300

    Feat: Criado tela de escolha de dowload

--- src/components/InputSearch.vue
+++ src/components/InputSearch.vue
@@ -24,8 +24,10 @@
 
       <div class="w-1.9">
         <p class="py-3 mb-1 text-sm font-bold">{{ title }}</p>
-        <br>
+
+        <p class="py-3 mb-1 text-sm font-bold grid grid-cols-1" v-show="text">Deseja baixar?</p>
         <div class="flex justify-center gap-3" v-show="show">
+
           <div
             class="bg-cont flex justify-center align-baseline gap-2 rounded-md border-2 border-500 w-40 h-12 cursor-pointer ..."
             @click="VideoCont">
@@ -38,18 +40,32 @@
             @click="AudioCont">
             <p class="py-3 mb-1 text-sm font-extrabold">Audio</p>
             <span class="py-3" v-html="svgAudio"></span>
-          </div>  
+          </div>
         </div>
         <div class="flex w-1.9 justify-center gap-3 py-3">
+          <div class="grid grid-cols-1 gap-3">
+            <div class="grid grid-rows-1">
+              <div class="flex justify-center gap-3">
+                <div v-for="(item, index) in container" :key="index"
+                  class="p-4 bg-gray-200 cursor-pointer rounded hover:bg-gray-500" :class="{ 'selected': isSelected(item) }"
+                  @click="toggleSelection(item)">
+                  {{ item }}
+                </div>
 
-          <div v-for="(item, index) in container" :key="index" class="p-4 bg-gray-200 cursor-pointer hover:bg-gray-500">
-              {{ item }}
+              </div>
             </div>
-            <br>
+            <div class="grid grid-rows-1">
+              <div class="flex gap-3">
+                <div v-for="(item, index) in resolution" :key="index"
+                  class="p-4 bg-gray-200 cursor-pointer rounded  hover:bg-gray-500" :class="{ 'selected': isSelected(item) }"
+                  @click="toggleSelection1(item)">
+                  {{ item }}p
+                </div>
 
-            <div v-for="(item, index) in resolution" :key="index" class="p-4 bg-gray-200 cursor-pointer  hover:bg-gray-500">
-              {{ item }}p
+              </div>
+
             </div>
+          </div>
         </div>
       </div>
     </div>
@@ -66,12 +82,16 @@ export default {
       extraText: "https://www.youtube.com/watch?v=gNBKvpAPMUI&list",
       Link: "",
       title: '',
+      cont: '',
+      res: '',
       obj: [],
       objVideo: [],
       container: [],
       resolution: [],
       objAudio: [],
       show: false,
+      text:false,
+      selectedItems: [],
       videoUrl: '',
       svgIcon: "<svg xmlns=\"http://www.w3.org/2000/svg\" fill=\"none\" viewBox=\"0 0 24 24\" stroke-width=\"1.5\" stroke=\"currentColor\" class=\"w-6 h-6\"><path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z\" /></svg>",
       svgVideo: "<svg xmlns=\"http://www.w3.org/2000/svg\" fill=\"none\" viewBox=\"0 0 24 24\" stroke-width=\"1.5\" stroke=\"currentColor\" class=\"w-6 h-6\"><path stroke-linecap=\"round\" d=\"M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z\" /></svg>",
@@ -84,11 +104,12 @@ export default {
       const url = this.Link.replace("watch?v=", "embed/")
       console.log(url)
       const result = await GetVideoUrl(url)
-      console.log(result.data)
+      
 
       if (result.status === 200) {
         this.videoUrl = url
         this.show = true
+        this.text = true
         // Filtrar apenas os objetos do tipo "mixed"
         const objMixed = this.obj = result.data.quality.filter(mixed => mixed.type === 'Mixed');
         this.objVideo = objMixed
@@ -98,9 +119,7 @@ export default {
         const arr = result.data;
         //Receber Titulo do video
         this.title = arr.title;
-        console.log(arr)
-        console.log(objMixed)
-        console.log(objAudios)
+       
       }
       else {
         console.log("Erro intero no servidor")
@@ -113,17 +132,49 @@ export default {
       const resolution = this.objVideo.map(res => res.resolution)
       this.container = container.filter((i, index) => container.indexOf(i) === index)
       this.resolution = resolution
-      console.log(container)
-      console.log(resolution)
+
     },
     AudioCont() {
       console.log("Audio")
-      console.log(this.objAudio)
-    }
-  },
+      const container = this.objAudio.map(cont => cont.container)
+      const bitRate = this.objAudio.map(bit => bit.bitrate)
+      this.container = container.filter((i, index) => container.indexOf(i) === index)
+      this.resolution = bitRate.slice(0, 3)
+    },
 
+    isSelected(item) {
+      return this.selectedItems.includes(item);
+    },
+    toggleSelection(item) {
+      if (this.isSelected(item)) {
+        this.selectedItems = this.selectedItems.filter(i => i !== item);
+      } else {
+        this.selectedItems.push(item);
+      }
+      const a = this.cont = this.selectedItems[0];
+
+      
+
+    },
+
+    toggleSelection1(item) {
+      if (this.isSelected(item)) {
+        this.selectedItems = this.selectedItems.filter(i => i !== item);
+      } else {
+        this.selectedItems.push(item);
+      }
+
+      const b = this.res = this.selectedItems[1];
+
+      
+    }
+
+  }
 };
 </script>
 
-
-     
+<style scoped>
+.selected {
+  background-color: yellow;
+}
+</style>
