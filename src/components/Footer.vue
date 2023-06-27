aebdd2f9778c84b3a452e084eeef20b689dd3d55
Matheus Filipe <theuslipe26@gmail.com>
2023-06-27 18:08:54 -0300

    Refactor: corrcção na posição do footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container-footer">
-        <div class=" border-2 border-indigo-0 py-1">
-            <h1 class="text-sm font-bold">©️ YouTubeMadDownload</h1>
+        <div class=" border-2 border-indigo-0 py-1 dark:border-d">
+            <h1 class="text-sm font-bold dark:text-white tracking-tight">©️ YouTubeMadDownload</h1>
         </div>
     </div>
 </template>
@@ -9,11 +9,10 @@
 
 <style scoped>
 .container-footer{
-    position: fixed;
+    position: relative;
     left: 0;
     bottom: 0;
     width: 100%;
-    border-color: #8080F1;
 }
 
 </style>
